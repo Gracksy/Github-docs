022d6bfa8d53c9b84d235ac5a831b8876dd47e9a
Gracksy <snampewo@gmail.com>
2015-07-29 11:33:52 +0300

    Assignment 4

--- Assignment 4.py
+++ Assignment 4.py
@@ -1,13 +1,11 @@
+files=("Sociology", "Research", "Development")
+
 def open_file():
 	file=open("C:\Users\User\Documents\python-tutorials\assignment4.txt", "r")
 	data=file.readlines
 	return data
 
-name=raw_input("Name: ")
-course=raw_input("Course: ")
-Course_unit=raw_input("Course_unit 1: ")
-grade=raw_input("Course_grade: ")
-counter=0
+list=("Sociology", "Research", "Development")
 
 def computed_grade(Course_grade):
 	grade_value= None
@@ -15,7 +13,7 @@ def computed_grade(Course_grade):
 	index=0
 	for g in grade_list:
 		index=index+1
-		if user_grade == g:
+		if Course_grade == g:
 			grade_value=index
 		if grade_value== None:
 			print("wrong grade; Please re-enter your grade")
@@ -33,8 +31,8 @@ def determining_performance (grade):
 		print("poor performance")
 
 def exam():
-	raw_grade = course()
-	grade_value=computed_grade(raw_grade)
+	raw_grade = list()
+	grade_value = computed_grade(raw_grade)
 	determining_performance(grade_value)
 
 
